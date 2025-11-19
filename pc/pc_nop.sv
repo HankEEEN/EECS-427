0d2c167fe5f7921700efce6009c722334e0d7f97
Zihan <zihanfu@eecs2420p18.engin.umich.edu>
2025-11-19 16:51:02 -0500

    fix signed extension issue

--- pc/pc_nop.sv
+++ pc/pc_nop.sv
@@ -44,7 +44,7 @@ logic           [ADDR_WIDTH-1:0]    w_pc_next;
 
 assign w_rf_addr = ~i_rf_addr_n;
 assign w_pc_plus1 = r_pc + 1'b1;
-assign w_br_addend = i_bcond ? {{OFFSET_WIDTH{i_disp[OFFSET_WIDTH-1]}}, i_disp} : 'd0;
+assign w_br_addend = i_bcond ? {{(ADDR_WIDTH-OFFSET_WIDTH){i_disp[OFFSET_WIDTH-1]}}, i_disp} : 'd0;
 assign w_pc_addout = w_br_addend + w_pc_plus1;
 
 always_comb begin
