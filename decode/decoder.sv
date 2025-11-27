28b6e320c96e10c9838ae0b9b6d27d2b1c2e242f
Zihan <zihanfu@eecs2420p11.engin.umich.edu>
2025-11-27 07:50:20 -0500

    upload new decoder.sv

--- decode/decoder.sv
+++ decode/decoder.sv
@@ -2,94 +2,102 @@
 // Save some control bits for more than one cycle (e.g. write_enable)
 // Make some or all of the flip-flops scannable (scan IR and PSR) - If you dont have an external program memory interface
 
+// BUGFIX:
+// 1. Replace rst with active-low reset rstn
+// 2. Replace Z with Z_B(inverted Z) since we receive Z_bar from the alu output
+// 3. inst <= {inst_i[14:0], scan_i} should have been inst <= {inst[14:0], scan_i}
+// 4. Fix GE: (N==0 || Z_B==1) (Should (N==1 || Z_B==0)) for bcond and jcond
+// 5. Fix Jcond FS typo (bcond_o --> jcond_o)
+// 6. With synchronous IMEM and DMEM, nop logic is needed
+// 7. CMP and CMPI affect only different PSR flags and do not write back any result, rf_we should be 0
+
 module decoder(
     
-    input logic [15:0] inst_in,
+    input logic [15:0] inst_i,
     input logic clk, 
-    input logic rst, 
+    input logic rstn, 
 
     // ALU flags
-    input logic Z_in, 
-    input logic N_in, 
-    input logic F_in,
+    input logic Z_i, 
+    input logic N_i, 
+    input logic F_i,
 
     // Scan Chain 
-    input logic scan_en, 
-    input logic scan_in, 
-    output logic scan_out, 
+    input logic scan_en_i, 
+    input logic scan_i, 
+    output logic scan_o, 
 
     // for RF 
-    output logic [15:0] r_dest, 
-    output logic [15:0] r_src, 
-    output logic rf_we,
+    output logic [15:0] r_dest_o, 
+    output logic [15:0] r_src_o, 
+    output logic rf_we_o,
 
     // for alu
-    output logic [3:0] alu_op, 
-    output logic sub, 
+    output logic [3:0] alu_op_o, 
+    output logic sub_o, 
 
     // for shfiter 
-    output logic [7:0] imm,
+    output logic [7:0] imm_o,
 
     // for pc 
-    output logic bcond, 
-    output logic jcond,
+    output logic bcond_o, 
+    output logic jcond_o,
+
+    // for store
+    output logic dmem_we, 
 
     // control mux signals
-    output logic alu_imm,
-    output logic mov, 
-    output logic lui, 
-    output logic mem, 
-    output logic alu, 
-    output logic pc, 
-    output logic shift, 
-    output logic lsh, 
-    output logic lshi 
+    output logic alu_imm_o,
+    output logic mov_o, 
+    output logic lui_o, 
+    output logic mem_o, 
+    output logic alu_o, 
+    output logic pc_o, 
+    output logic shift_o, 
+    output logic lsh_o, 
+    output logic lshi_o 
 
 ); 
 
-    logic        Z, N, F; 
-    logic [3:0]  opcode, op_ext, cond, r_dest_shift, r_src_shift; 
-    logic [15:0] inst; 
-    
+    localparam logic [15:0] NOP = 16'h0020;
 
-    typedef enum logic [3:0] {
+    logic        Z_B, N, F, rf_we; 
+    logic [3:0]  r_dest_shift, r_src_shift; 
+    logic [15:0] inst;
+    logic        flush;
+
+    typedef enum logic unsigned [3:0] {
         ALU = 4'h0, // look at OP Code Ext
         ANDI = 4'h1, 
         ORI = 4'h2,
         XORI = 4'h3,
         MEM = 4'h4, // look at OP Code Ext 
         ADDI = 4'h5, 
-        LSHI = 4'h8, 
+        SHIFT = 4'h8, 
         SUBI = 4'h9, 
         CMPI = 4'hb, 
         BCOND = 4'hc, 
         MOVI = 4'hd, 
-        // MULI = 4'he 
         LUI = 4'hf 
-    } opcodes; 
+    } opcode_e; 
 
-    /* everything // is unused */
 
     typedef enum logic unsigned [3:0] { 
         AND = 4'h1, 
         OR = 4'h2,
         XOR = 4'h3, 
-        LSH = 4'h4,
+        STOR = 4'h4, 
         ADD = 4'h5, 
         SUB = 4'h9, 
         CMP = 4'hb, 
-        MOV = 4'hd  
-        //  MUL = 4'he 
-    } op_ext_alu; 
-
-    typedef enum logic unsigned [3:0] {
-        LOAD = 4'h0,
-        STOR = 4'h4, 
+        MOV = 4'hd,
+        LOAD = 4'h0, 
         JAL = 4'h8, 
         JCOND = 4'hc  
-    } op_ext_mem; 
+    } op_ext_e; 
+    
 
-    typedef enum logic unsigned[3:0] {
+    typedef enum logic unsigned [3:0] {
         EQ = 4'h0,
         NE = 4'h1, 
         GT = 4'h6,  
@@ -98,34 +106,44 @@ module decoder(
         FC = 4'h9, 
         LT = 4'hc,
         GE = 4'hd 
-    } branch_conditions; 
+    } branch_condition_e; 
+    
+    opcode_e opcode;
+    op_ext_e op_ext;
+    branch_condition_e branch_condition;
     
 
     // INSTRUCTION REGISTER + FLIPFLOPS
     always_ff @(posedge clk) begin 
-        if(rst) begin 
-            Z    <= 0;
+        if(!rstn) begin 
+            Z_B    <= 1;
             F    <= 0; 
             N    <= 0; 
             inst <= '0;  
-        end else if(scan_en) begin
-            Z    <= 0;
+            rf_we_o <= 0; 
+        end else if(scan_en_i) begin
+            Z_B    <= 1;
             F    <= 0; 
-            N    <= 0; 
-            inst <= {inst[14:0], scan_in}; 
+            N    <= 0;
+            rf_we_o <= 0;  
+            inst <= {inst[14:0], scan_i};
+        end else if (flush) begin
+            inst <= NOP;
+            rf_we_o <= 0; 
         end else begin 
-            Z    <= Z_in; 
-            F    <= F_in; 
-            N    <= N_in;
-            inst <= inst_in[15:0]; 
+            Z_B    <= Z_i; 
+            F    <= F_i; 
+            N    <= N_i;
+            inst <= inst_i[15:0];
+            rf_we_o <= rf_we;  
         end 
     end 
 
     always_ff @(posedge clk) begin 
-        if (rst) begin 
-            scan_out <= 1'b0; 
+        if (!rstn) begin
+            scan_o <= 1'b0; 
         end else begin 
-            scan_out <= inst[15]; 
+            scan_o <= inst[15]; 
         end 
     end 
 
@@ -134,207 +152,226 @@ module decoder(
         opcode = inst[15:12]; 
         r_dest_shift = inst[11:8]; 
         r_src_shift  = inst[3:0]; 
-        cond   = inst[11:8]; 
+        branch_condition   = inst[11:8]; 
         op_ext = inst[7:4]; 
-        imm    = inst[7:0]; 
+        
+        imm_o    = inst[7:0]; 
 
-        sub = 1'b0;
+        sub_o = 1'b0;
         rf_we = 1'b0;  
-        alu_op = '0; 
+        alu_op_o = '0; 
 
-        alu_imm = 1'b0;
-        mov = 1'b0;
-        lui = 1'b0;
-        mem = 1'b0;
-        alu = 1'b0;
-        pc = 1'b0;
-        shift = 1'b0;
-        lsh = 1'b0;
-        lshi = 1'b0;
-        bcond = 1'b0; 
-        jcond = 1'b0; 
+        dmem_we = 1'b0; 
 
-        r_dest = 1'b1 << r_dest_shift; 
-        r_src = 1'b1 << r_src_shift; 
+        alu_imm_o = 1'b0;
+        mov_o = 1'b0;
+        lui_o = 1'b0;
+        mem_o = 1'b0;
+        alu_o = 1'b0;
+        pc_o = 1'b0;
+        shift_o = 1'b0;
+        lsh_o = 1'b0;
+        lshi_o = 1'b0;
+        bcond_o = 1'b0; 
+        jcond_o = 1'b0; 
+
+        r_dest_o = 1'b1 << r_dest_shift; 
+        r_src_o = 1'b1 << r_src_shift; 
 
 
         case(opcode)
 
             // IMMEDIATE INSTRUCTIONS
             ADDI: begin 
-                alu_op = 4'b0001; 
-                alu_imm = 1'b1; 
-                alu = 1'b1;
+                alu_op_o = 4'b0001; 
+                alu_imm_o = 1'b1; 
+                alu_o = 1'b1;
                 rf_we = 1'b1; 
-                lui = 1'b1;                
+                lui_o = 1'b1;                
             end 
 
             ANDI: begin 
-                alu_op = 4'b0010; 
-                alu_imm = 1'b1; 
-                alu = 1'b1;
+                alu_op_o = 4'b0010; 
+                alu_imm_o = 1'b1; 
+                alu_o = 1'b1;
                 rf_we = 1'b1; 
-                lui = 1'b1;                
+                lui_o = 1'b1;                
             end 
 
             ORI: begin 
-                alu_op = 4'b0100; 
-                alu_imm = 1'b1; 
-                alu = 1'b1;
+                alu_op_o = 4'b0100; 
+                alu_imm_o = 1'b1; 
+                alu_o = 1'b1;
                 rf_we = 1'b1; 
-                lui = 1'b1;                
+                lui_o = 1'b1;                
             end 
 
             XORI: begin 
-                alu_op = 4'b1000; 
-                alu_imm = 1'b1; 
-                alu = 1'b1;
+                alu_op_o = 4'b1000; 
+                alu_imm_o = 1'b1; 
+                alu_o = 1'b1;
                 rf_we = 1'b1; 
-                lui = 1'b1;                
+                lui_o = 1'b1;                
             end 
 
             SUBI: begin 
-                alu_op = 4'b0001;                         
-                sub = 1'b1; 
-                alu_imm = 1'b1; 
-                alu = 1'b1;
+                alu_op_o = 4'b0001;                         
+                sub_o = 1'b1; 
+                alu_imm_o = 1'b1; 
+                alu_o = 1'b1;
                 rf_we = 1'b1; 
-                lui = 1'b1;                
+                lui_o = 1'b1;                
             end 
 
             CMPI: begin 
-                alu_op = 4'b0001; 
-                sub = 1'b1; 
-                alu_imm = 1'b1; 
-                alu = 1'b1;
-                rf_we = 1'b1; 
-                lui = 1'b1;                
+                alu_op_o = 4'b0001; 
+                sub_o = 1'b1; 
+                alu_imm_o = 1'b1; 
+                alu_o = 1'b1;
+                rf_we = 1'b0; 
+                lui_o = 1'b1;                
             end
 
             // ALU INSTRUCITONS 
             ALU: begin
-                alu = 1'b1; 
+                alu_o = 1'b1; 
                 rf_we = 1'b1; 
 
                 case(op_ext)
                     ADD: begin 
-                        alu_op = 4'b0001; 
+                        alu_op_o = 4'b0001; 
                     end 
 
                     AND: begin 
-                        alu_op = 4'b0010; 
+                        alu_op_o = 4'b0010; 
                     end 
 
                     OR: begin 
-                        alu_op = 4'b0100; 
+                        alu_op_o = 4'b0100; 
                     end 
 
                     XOR: begin 
-                        alu_op = 4'b1000; 
+                        alu_op_o = 4'b1000; 
                     end 
 
                     SUB: begin 
-                        alu_op = 4'b0001;                         
-                        sub = 1'b1; 
+                        alu_op_o = 4'b0001;                         
+                        sub_o = 1'b1; 
                     end 
 
                     CMP: begin 
-                        alu_op = 4'b0001; 
-                        sub = 1'b1; 
+                        alu_op_o = 4'b0001; 
+                        sub_o = 1'b1; 
+                        rf_we = 1'b0; 
                     end
+                    
+                    MOV: begin 
+                        mov_o = 1'b1; 
+                        alu_op_o = 4'b0001; 
+                        alu_o = 1'b1; 
+                        rf_we = 1'b1; 
+                    end 
 
                     default: begin 
-
+                        alu_o = 1'b0; 
+                        rf_we = 1'b0;
                     end 
                 endcase
             end 
 
             // SHIFT INSTRUCTIONS
+            SHIFT: begin
+                shift_o = 1'b1;
+                rf_we = 1'b1;
 
-            MOV: begin 
-                mov = 1'b1; 
-                alu_op = 4'b0001; 
-                alu = 1'b1; 
-                rf_we = 1'b1; 
-            end 
+                casez(op_ext)
+                    4'b0100: begin  // LSH
+                        lsh_o = 1'b1;
+                    end
 
+                    4'b000?: begin  // LSHI
+                        lshi_o = 1'b1;
+                    end
+
+                    default: begin
+                        shift_o = 1'b0;
+                        rf_we = 1'b0;
+                    end
+
+                endcase
+            end
+            
+            
             MOVI: begin 
-                mov = 1'b1; 
-                lui = 1'b1; 
-                alu_imm = 1'b1; 
-                alu_op = 4'b0001;
-                alu = 1'b1;  
+                mov_o = 1'b1; 
+                lui_o = 1'b1; 
+                alu_imm_o = 1'b1; 
+                alu_op_o = 4'b0001;
+                alu_o = 1'b1;  
                 rf_we = 1'b1;       
-
-            end 
-
-            LSH: begin 
-                shift = 1'b1; 
-                rf_we = 1'b1; 
-                lsh = 1'b1; 
-            end 
-
-            LSHI: begin 
-                shift = 1'b1; 
-                rf_we = 1'b1; 
-                lshi = 1'b1; 
             end 
 
             LUI: begin 
-                shift = 1'b1;
-                lui = 1'b1; 
+                shift_o = 1'b1;
+                lui_o = 1'b1; 
                 rf_we = 1'b1; 
             end 
 
-            // BCOND IN CONTROLLER 
             MEM: begin 
                 case(op_ext) 
                     LOAD: begin 
-                        mem = 1'b1; 
+                        mem_o = 1'b1; 
                         rf_we = 1'b1; 
                     end 
 
-                    STOR: begin
-                        mem = 1'b1;  
+                    4'b0100: begin // STORE
+                        mem_o = 1'b1;  
                         rf_we = 1'b0; 
+                        dmem_we = 1'b1; 
                     end 
 
                     JAL: begin 
                         rf_we = 1'b1; 
-                        pc = 1'b1; 
-                        jcond = 1'b1; 
+                        pc_o = 1'b1; 
+                        jcond_o = 1'b1; 
                     end  
 
                     JCOND: begin
-                        case(cond)
+                        case(branch_condition)
                             EQ: begin 
-                                jcond = (Z==1) ? 1 : 0; 
+                                jcond_o = (Z_B==0) ? 1'b1 : 1'b0; 
                             end 
 
                             NE: begin 
-                                jcond = (Z==0) ? 1 : 0; 
-                            end 
-                            GT: begin 
-                                jcond = (N==1) ? 1 : 0; 
-                            end   
-                            LE: begin 
-                                jcond = (N==0) ? 1 : 0; 
-                            end 
-
-                            FS: begin 
-                                bcond = (F==1) ? 1 : 0; 
-                            end 
-
-                            FC: begin 
-                                jcond = (F==0) ? 1 : 0; 
-                            end 
-
-                            LT: begin 
-                                jcond = (N==0 && Z==0) ? 1 : 0; 
+                                jcond_o = (Z_B==1) ? 1'b1 : 1'b0; 
                             end 
 
                             GE: begin 
-                                jcond = (N==0 || Z==0) ? 1 : 0; 
+                                jcond_o = (N==1 || Z_B==0) ? 1'b1 : 1'b0; 
+                            end 
+
+                            GT: begin 
+                                jcond_o = (N==1) ? 1'b1 : 0; 
+                            end   
+
+                            LE: begin 
+                                jcond_o = (N==0) ? 1'b1 : 1'b0; 
+                            end 
+
+                            FS: begin 
+                                jcond_o = (F==1) ? 1'b1 : 1'b0; 
+                            end 
+
+                            FC: begin 
+                                jcond_o = (F==0) ? 1'b1 : 1'b0; 
+                            end 
+
+                            LT: begin 
+                                jcond_o = (N==0 && Z_B==1) ? 1'b1 : 1'b0; 
+                            end 
+                            
+                            default: begin 
+                            
                             end 
                         endcase
                     end 
@@ -343,37 +380,43 @@ module decoder(
 
             // might need FSM 
             BCOND: begin 
-                case(cond)
-                            EQ: begin 
-                                bcond = (Z==1) ? 1 : 0; 
-                            end 
+                case(branch_condition)
+                    EQ: begin 
+                        bcond_o = (Z_B==0) ? 1'b1 : 1'b0; 
+                    end 
 
-                            NE: begin 
-                                bcond = (Z==0) ? 1 : 0; 
-                            end 
-                            GT: begin 
-                                bcond = (N==1) ? 1 : 0; 
-                            end   
-                            LE: begin 
-                                bcond = (N==0) ? 1 : 0; 
-                            end 
+                    NE: begin 
+                        bcond_o = (Z_B==1) ? 1'b1 : 1'b0; 
+                    end 
 
-                            FS: begin 
-                                bcond = (F==1) ? 1 : 0; 
-                            end 
+                    GE: begin 
+                        bcond_o = (N==1 || Z_B==0) ? 1'b1 : 1'b0; 
+                    end
 
-                            FC: begin 
-                                bcond = (F==0) ? 1 : 0; 
-                            end 
+                    GT: begin 
+                        bcond_o = (N==1) ? 1'b1 : 1'b0; 
+                    end   
 
-                            LT: begin 
-                                bcond = (N==0 && Z==0) ? 1 : 0; 
-                            end 
+                    LE: begin 
+                        bcond_o = (N==0) ? 1'b1 : 1'b0; 
+                    end 
 
-                            GE: begin 
-                                bcond = (N==0 || Z==0) ? 1 : 0; 
-                            end 
-                        endcase
+                    FS: begin 
+                        bcond_o = (F==1) ? 1'b1 : 1'b0; 
+                    end 
+
+                    FC: begin 
+                        bcond_o = (F==0) ? 1'b1 : 1'b0; 
+                    end 
+
+                    LT: begin 
+                        bcond_o = (N==0 && Z_B==1) ? 1'b1 : 1'b0; 
+                    end 
+                    
+                    default: begin 
+                    
+                    end  
+                endcase
             end 
 
             default: begin 
@@ -381,6 +424,8 @@ module decoder(
             end  
 
         endcase 
+
+        flush = bcond_o || jcond_o;
 
     end 
 
